0cdd6609b1d2c5750ea8115a1cb2e8ef73d6b04d
jerrypanda4563 <125418322+jerrypanda4563@users.noreply.github.com>
2024-02-29 23:49:26 +0000

    wait 20s between simulations, no threads in runner

--- app/internal/runner.py
+++ app/internal/runner.py
@@ -1,7 +1,7 @@
 import traceback
 import json
 from typing import Dict, Optional
-
+import time
 
 import app.mongo_config as mongo_db
 
@@ -26,36 +26,55 @@ def run_single_simulation(survey: Dict, demographic_profile: Dict, sim_id: str) 
     
 
 
-
-
-
 def run_simulation(survey: Dict, demographic_parameters: Dict, n_of_runs: int, sim_id: str, n_workers: Optional[int]=5) -> bool:
-    
-    database = mongo_db.collection_simulations
     demographic_generator=Demographic_Generator(demo=demographic_parameters, n_of_results=n_of_runs)
     demographic_profiles=demographic_generator.generate_demographic_dataset()
     print("demographic profiles generated")
+    
     n_of_completed_runs = 0
-    #### sth is causing the threads to be non-independent
-    try:
-        with ThreadPoolExecutor(max_workers=n_workers) as executor:
-            future_to_simulation = {executor.submit(run_single_simulation,survey, demo, sim_id): demo for demo in demographic_profiles}
-            for future in as_completed(future_to_simulation):
-                n_of_completed_runs += 1
-                print(f"Completed {n_of_completed_runs} of {n_of_runs} runs.")
-                if n_of_completed_runs == n_of_runs:
-                    run_status = False
-                    database.update_one(query, {"$set":{"Run Status": run_status}})
-                    print(f"All runs completed for simulation {sim_id}.")
-                    return True
+    for demo in range(demographic_profiles):
+        run_single_simulation(survey, demo, sim_id)
+        n_of_completed_runs += 1
+        print(f"Completed {n_of_completed_runs} of {n_of_runs} runs.")
+        time.sleep(20)
+
+        if n_of_completed_runs == n_of_runs:
+            run_status = False
+            query = {"_id": sim_id}
+            database = mongo_db.collection_simulations
+            database.update_one(query, {"$set":{"Run Status": run_status}})
+            print(f"All runs completed for simulation {sim_id}.")
+            return True
+
+
+
+# def run_simulation(survey: Dict, demographic_parameters: Dict, n_of_runs: int, sim_id: str, n_workers: Optional[int]=5) -> bool:
+    
+#     database = mongo_db.collection_simulations
+#     demographic_generator=Demographic_Generator(demo=demographic_parameters, n_of_results=n_of_runs)
+#     demographic_profiles=demographic_generator.generate_demographic_dataset()
+#     print("demographic profiles generated")
+#     n_of_completed_runs = 0
+#     #### sth is causing the threads to be non-independent
+#     try:
+#         with ThreadPoolExecutor(max_workers=n_workers) as executor:
+#             future_to_simulation = {executor.submit(run_single_simulation,survey, demo, sim_id): demo for demo in demographic_profiles}
+#             for future in as_completed(future_to_simulation):
+#                 n_of_completed_runs += 1
+#                 print(f"Completed {n_of_completed_runs} of {n_of_runs} runs.")
+#                 if n_of_completed_runs == n_of_runs:
+#                     run_status = False
+#                     database.update_one(query, {"$set":{"Run Status": run_status}})
+#                     print(f"All runs completed for simulation {sim_id}.")
+#                     return True
                 
-    except Exception as e:
-        print(f"Thread pool was unexpectedly terminated: {e}.")
-        traceback.print_exc()
-        query = {"_id": sim_id}
-        run_status = False
-        database.update_one(query, {"$set":{"Run Status": run_status}})
-        return False
+#     except Exception as e:
+#         print(f"Thread pool was unexpectedly terminated: {e}.")
+#         traceback.print_exc()
+#         query = {"_id": sim_id}
+#         run_status = False
+#         database.update_one(query, {"$set":{"Run Status": run_status}})
+#         return False
 
 # def run_simulation(survey: Dict, demographic_parameters: Dict, n_of_runs: int, sim_id: str, n_workers: Optional[int]=5) -> bool:
     
