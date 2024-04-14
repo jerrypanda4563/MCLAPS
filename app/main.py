4e4d977c01d641cb67376d58a5ca0ebed2d7e350
jerrypanda4563 <125418322+jerrypanda4563@users.noreply.github.com>
2024-04-14 15:54:18 +0100

    type for new_response client method input to dict

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ async def test_services():
     mclapsrl_status = mclapsrl_client.check_service_status()
     return {"OpenAI Status": str(openai_status), "Mongo Status": str(mongo_status), "Redis Status": str(redis_status), "RateLimiter Status": mclapsrl_status}
 
-@application.get("/mclapsrl/new_response")
+@application.post("/mclapsrl/new_response")
 async def new_response(response_body: str):
     mclapsrl_client = mclapsrl.mclapsrlClient()
     response = mclapsrl_client.new_response(response_body)
