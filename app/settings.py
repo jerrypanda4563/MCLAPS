5d40de98da88d0eca3ddf8fe927e4b76916eb162
jerrypanda4563 <125418322+jerrypanda4563@users.noreply.github.com>
2024-04-25 16:47:09 +0100

    updated settings definition for extracting environment variables

--- app/settings.py
+++ app/settings.py
@@ -3,9 +3,9 @@ import os
 
 load_dotenv()
 
-OPEN_AI_KEY = os.getenv("OPEN-AI-API-KEY")
-MONGO_URI= os.getenv("MONGO-URI")
-REDIS_URI = os.getenv("REDIS-URI")
-REDIS_PORT = os.getenv("REDIS-PORT")
-MCLAPSRL_API = os.getenv("MCLAPSRL-URI")
+OPEN_AI_KEY = os.getenv("OPEN_AI_API_KEY")
+MONGO_URI= os.getenv("MONGO_URI")
+REDIS_URI = os.getenv("REDIS_URI")
+REDIS_PORT = os.getenv("REDIS_PORT")
+MCLAPSRL_API = os.getenv("MCLAPSRL_URI")
 DEBUG = os.getenv("DEBUG") == 'True'
