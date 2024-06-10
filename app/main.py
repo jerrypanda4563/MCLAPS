87c6e60f9eae2f37102de63dd0319b13786ff172
jerrypanda4563 <125418322+jerrypanda4563@users.noreply.github.com>
2024-06-10 13:26:28 +0100

    timeout and ttl = 7200s

--- app/main.py
+++ app/main.py
@@ -100,7 +100,7 @@ async def new_simulation(sim_param: SimulationParameters):
             request_batches = [n_of_runs]
 
         try:
-            tasks = [queue.enqueue(runner.run_simulation, args = (sim_id, survey_object, demographic_params, agent_params, batch_sample_size, n_of_workers), retry = rq.Retry(max=3, interval = 10), results_ttl = 3600) for batch_sample_size in request_batches]
+            tasks = [queue.enqueue(runner.run_simulation, args = (sim_id, survey_object, demographic_params, agent_params, batch_sample_size, n_of_workers), retry = rq.Retry(max=3, interval = 10), results_ttl = 7200, timeout = 7200) for batch_sample_size in request_batches]
         except Exception as e:
             raise HTTPException(status_code=400,detail=f'Failed to initiate simulation task: {e}.') 
         
